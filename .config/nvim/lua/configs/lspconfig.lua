63ce9dc73b94696981685fc00ff9942e6845f510
Pramod Phuyal <iampramodvii@gmail.com>
2024-10-04 00:50:01 +0545

    Rofi, configs

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "intelephense", "pyright", "tsserver" }
+local servers = { "html", "cssls", "intelephense", "pyright", "ts_ls" }
 -- lsps with default config
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup {
@@ -14,7 +14,7 @@ for _, lsp in ipairs(servers) do
     }
 end
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
     on_attach = on_attach,
     on_init = on_init,
     capabilities = capabilities,
